9156825c041d784cc0ef4e3c4b43804430aaa3b8
Andriw Gabryel <agfmondego5@gmail.com>
2026-03-12 15:41:38 -0300

    Upload projeto

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mod_acad.wsgi --workers 2
--- mod_acad/settings.py
+++ mod_acad/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 """
 Django settings for mod_acad project.
@@ -25,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pn$hgl&@7^b@8khec!#4%tjdph#nrubqrhm0j@q66o4_%!$)l5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*'] #10.0.51.122
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'withenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,10 +79,11 @@ WSGI_APPLICATION = 'mod_acad.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        
+        )
 }
 
 
@@ -120,6 +123,8 @@ TIME_ZONE = 'America/Fortaleza'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
